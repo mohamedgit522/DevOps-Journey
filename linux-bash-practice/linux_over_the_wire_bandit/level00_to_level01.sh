2d9634ef7fc4de69baaa252614f88d4047e5a837
mohamedgit522 <sh.mohamedahmed6@gmail.com>
2025-07-30 11:27:50 +0100

    changed level00 to level01 to an MD file

--- linux-bash-practice/linux_over_the_wire_bandit/level00_to_level01.sh
+++ linux-bash-practice/linux_over_the_wire_bandit/level00_to_level01.sh
@@ -1,26 +1,34 @@
-#!/bin/bash
-# Bandit Level 0 → Level 1
-#
-# Goal:
-# Retrieve the password for Level 1.
-# It is stored in a file named "readme" in the home directory.
-#
-# Steps:
-# 1. SSH into the Bandit server as bandit0.
-#    ssh bandit0@bandit.labs.overthewire.org -p 2220
-#
-# 2. List files to find "readme".
-#    ls
-#
-# 3. Display contents of "readme".
-#    cat readme
-#
-# Password:
-# cvX2JDeQyF5rDZ6RX5dmPQnNlzTnBqC7
-#
-# Takeaways:
-# - Basic SSH connection.
-# - Listing files and reading file content.
+# Bandit Level 0 → Level 1
 
-ssh bandit0@bandit.labs.overthewire.org -p 2220 "cat readme"
+## The Goal
+Retrieve the password for Level 1. It’s stored in a file named `readme` in the home directory.
 
+---
+
+## My Solution
+
+**Step 1:** Log into the remote server  
+
+```
+ssh bandit0@bandit.labs.overthewire.org -p 2220
+
+```
+**Step 2:** List files in the home directory to find `readme`
+
+```
+ls
+
+```
+**Step 3:** Display the contents of `readme` to reveal the password
+
+**Password for Level 1:**
+
+```
+cvX2JDeQyF5rDZ6RX5dmPQnNlzTnBqC7
+
+```
+
+**Takeaways:**
+
+- Practiced basic SSH login.
+- Navigated remote directory and viewed file contents.
